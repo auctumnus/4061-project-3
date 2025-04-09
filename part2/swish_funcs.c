8d4d74529ed665c14fa0018cdce51b4ea7e4f51d
kholodets <eliza@kholo.moe>
2025-04-09 11:28:34 -0500

    pre-testing

--- part2/swish_funcs.c
+++ part2/swish_funcs.c
@@ -26,11 +26,123 @@
  * Returns 0 on success or -1 on error.
  */
 int run_piped_command(strvec_t *tokens, int *pipes, int n_pipes, int in_idx, int out_idx) {
-    // TODO Complete this function's implementation
+    int fret = fork();
+    if (fret == -1) {
+	    perror("fork");
+	    return -1;
+    } else if (fret == 0) {
+	    //child
+	    for (int i = 0; i < n_pipes; i++) {
+		    if (i != in_idx && i != out_idx) {
+			    if (close(pipes[i])) {
+				    perror("close");
+				    exit(1);
+			    }
+		    }
+	    }
+
+	    int pidx = strvec_find(tokens, "|");
+	    if (pidx > 0) {
+		    strvec_take(tokens, pidx);
+	    }
+
+	    if (in_idx != -1) {
+		    if (dup2(pipes[in_idx], STDIN_FILENO)) {
+			    perror("pipe");
+			    exit(1);
+		    }
+	    }
+
+	    if (out_idx != -1) {
+		    if (dup2(pipes[out_idx], STDOUT_FILENO)) {
+			    perror("pipe");
+			    exit(1);
+		    }
+	    }
+
+	    if (run_command(tokens)) {
+		    fprintf(stderr, "run_command failed\n");
+		    exit(1);
+	    }
+
+    } else if (fret > 0) {
+	    //parent
+	    return 0;
+    }
     return 0;
 }
 
 int run_pipelined_commands(strvec_t *tokens) {
-    // TODO Complete this function's implementation
+    int n_pipes = strvec_num_occurrences(tokens, "|");
+    int *pfds = malloc(sizeof(int) * n_pipes * 2);
+    if (pfds == NULL && n_pipes > 0) { // malloc might return NULL if the length is 0, which is fine
+	    perror("malloc");
+	    return -1;
+    }
+    for (int i = 0; i < n_pipes; i++) {
+	    if (pipe(pfds + (i*2))) {
+		    perror("pipe");
+		    for (int j = 0; j < i*2; j++) {
+			    close(pfds[j]);
+			    close(pfds[j+1]);
+		    }
+		    free(pfds);
+		    return -1;
+	    }
+    }
+
+    strvec_t cur;
+    if (strvec_slice(tokens, &cur, 0, tokens->length)) {
+	    fprintf(stderr, "strvec slice failed\n");
+	    for (int i = 0; i < n_pipes * 2; i++) {
+		    close(pfds[i]);
+	    }
+	    free(pfds);
+	    return -1;
+    }
+
+    for (int i = 0; i <= n_pipes; i++) {
+	    int in_idx = (i == 0) ? -1 : (i-1) * 2;
+	    int out_idx = (i == n_pipes) ? -1 : i * 2 + 1;
+	    int length = n_pipes * 2;
+
+	    if (run_piped_command(tokens, pfds, length, in_idx, out_idx)) {
+		    fprintf(stderr, "failed to run one command in chain\n");
+		    for (int j = 0; j < length; j++) {
+			    close(pfds[j]);
+		    }
+		    free(pfds);
+		    return -1;
+	    }
+
+	    if (i != n_pipes) {
+		    strvec_t temp;
+		    int pidx = strvec_find(&cur, "|");
+		    if (strvec_slice(&cur, &temp, pidx, cur.length)) {
+			    fprintf(stderr, "strvec slice failed\n");
+			    for (int j = 0; j < length; j++) {
+				    close(pfds[i]);
+			    }
+			    free(pfds);
+			    return -1;
+		    }
+		    strvec_clear(&cur);
+		    cur = temp;
+	    }
+    }
+
+    for (int i = 0; i < n_pipes * 2; i++) {
+	    if (close(pfds[i])) {
+		    perror("close");
+		    for (int j = i+1; j < n_pipes * 2; j++) {
+			    close(pfds[j]);
+		    }
+		    free(pfds);
+		    return -1;
+	    }
+    }
+
+    free(pfds);
+
     return 0;
 }
